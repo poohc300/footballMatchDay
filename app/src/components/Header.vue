f19cebb2773fa0de73ac9234ac392e2cdd77d311
jeremy <jmkim12@i-gns.co.kr>
2025-02-28 16:59:57 +0900

    디렉토리명 수정

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
     <v-app-bar app>
       <v-toolbar-title>My App</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text to="/schedule">경기목록</v-btn>
+      
     </v-app-bar>
   </template>
   
--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -5,6 +5,9 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default createVuetify({
+    theme: {
+        defaultTheme: 'light', // 기본 테마를 라이트 테마로 설정
+    },
     components,
     directives
 })
